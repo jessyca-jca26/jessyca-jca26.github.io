402cc0e27a7e9656275220a4e98d3519216337c9
jessyca-jca26 <jessyca.jca26@gmail.com>
2022-04-19 17:05:33 -0300

    Programação Formspree

--- simplicity/js/scripts.js
+++ simplicity/js/scripts.js
@@ -89,3 +89,40 @@ textMensagem.addEventListener("input", function () {
     }
 
 });
+
+
+
+
+
+
+/* Programação do Formspree */
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Obrigado por enviar!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! deu ruim! Tente novamente mas tarde"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
